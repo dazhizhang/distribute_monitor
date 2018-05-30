ab9f0d7ada8eb53ab23859f9cd0f4ce685ab19ac
dazhizhang <lifeoug@gmail.com>
2018-05-30 11:30:49 -0700

    Add files via upload

---
+++ modifyprice.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- 主机: 127.0.0.1
+-- 生成日期: 2018 年 05 月 30 日 20:30
+-- 服务器版本: 5.5.32
+-- PHP 版本: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `monitor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `modifyprice`
+--
+
+CREATE TABLE IF NOT EXISTS `modifyprice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(100) NOT NULL,
+  `lastupdate` varchar(100) NOT NULL,
+  `apivalid` varchar(100) DEFAULT NULL,
+  `note` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
